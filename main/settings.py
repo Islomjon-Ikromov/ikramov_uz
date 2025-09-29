6b784e367324ce5c8dcb9431d4d543ec241b9586
Islomjon-Ikromov <mcxacker7@gmail.com>
2025-09-29 20:24:28 +0500

    update

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure--r5l0^n(l1an7b!iu(is#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', "ikramov.uz"]
 
 
 # Application definition
