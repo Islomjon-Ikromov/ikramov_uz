5370c7db6eb272c3105a21de3b06072a9b5201d2
Islomjon-Ikromov <mcxacker7@gmail.com>
2025-09-29 20:40:24 +0500

    Remove Whitenoise middleware and static file storage configuration from settings.py as part of the project refactor.

--- main/settings.py
+++ main/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,9 +124,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
-# Use WhiteNoise static files storage in production (DEBUG=False)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
